01d3ed7e8c8ba4a9ada2d086693c2226268c4f7d
Nick <nick.ciraki@gmail.com>
2022-03-21 12:39:11 +0000

    getAll function working. Renamed test-data.sql to data.sql for H2 to work

--- DFESW13-Pokemon/src/main/java/com/qa/Pokemon/controller/Controller.java
+++ DFESW13-Pokemon/src/main/java/com/qa/Pokemon/controller/Controller.java
@@ -1,7 +1,10 @@
 package com.qa.Pokemon.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +28,12 @@ public class Controller {
 		services.createMon(mon);
 		String response = mon.getName() + " added!";
 		return new ResponseEntity<String>(response, HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/getAll")
+	public ResponseEntity<List<Pokemon>> getAll(){
+		
+		List<Pokemon> result = services.getAll();
+		return new ResponseEntity<>(result, HttpStatus.ACCEPTED);
 	}
 }
--- DFESW13-Pokemon/src/main/java/com/qa/Pokemon/services/Services.java
+++ DFESW13-Pokemon/src/main/java/com/qa/Pokemon/services/Services.java
@@ -1,5 +1,7 @@
 package com.qa.Pokemon.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.qa.Pokemon.model.Pokemon;
@@ -19,6 +21,10 @@ public class Services {
 		Pokemon savedObj = repo.save(mon);
 		return savedObj;
 		
+	}
+
+	public List<Pokemon> getAll() {
+		return repo.findAll();
 	}
 
 }
