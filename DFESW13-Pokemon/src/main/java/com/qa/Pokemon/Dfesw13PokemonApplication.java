d1514b207d85a9ea4fdbdbf5816d817f8d57de3a
Nick <nick.ciraki@gmail.com>
2022-03-21 10:58:18 +0000

    Dev branch created

---
+++ DFESW13-Pokemon/src/main/java/com/qa/Pokemon/Dfesw13PokemonApplication.java
@@ -0,0 +1,13 @@
+package com.qa.Pokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dfesw13PokemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dfesw13PokemonApplication.class, args);
+	}
+
+}
